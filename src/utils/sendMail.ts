ced052586fdb5e35bc018e6bd3cd739013bf8f92
Natã Hermes <nhermesdev@gmail.com>
2023-07-05 00:38:08 -0300

    feat: add email send with oauth 2.0 googleapis

--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -1,29 +1,56 @@
 import nodemailer from 'nodemailer';
+import { google } from 'googleapis';
+const OAuth2 = google.auth.OAuth2;
 
-interface SendMailProps {
+interface SendMailOptions {
   to: string;
   subject: string;
-  text: string;
+  html: string;
 }
 
-export const sendEmail = async ({ to, subject, text }: SendMailProps) => {
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    'https://developers.google.com/oauthplayground',
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN,
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject();
+      }
+      resolve(token);
+    });
+  });
+
   const transporter = nodemailer.createTransport({
+    //@ts-ignore
     service: 'gmail',
     auth: {
+      type: 'OAuth2',
       user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASSWORD,
+      accessToken,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
     },
   });
 
-  try {
-    const mailOptions = {
-      from: process.env.EMAIL,
-      to: to,
-      subject: subject,
-      html: text,
-    };
+  return transporter;
+};
 
-    const info = await transporter.sendMail(mailOptions);
+export const sendEmail = async (sendEmailOptions: SendMailOptions) => {
+  try {
+    let emailTransporter = await createTransporter();
+    const info = await emailTransporter.sendMail({
+      ...sendEmailOptions,
+      from: process.env.EMAIL,
+    });
     return {
       kind: 'ok',
       info,
